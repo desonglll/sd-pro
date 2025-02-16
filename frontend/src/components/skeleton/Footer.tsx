aa44e8d1afc9ed3fee84a3a83268b9e466dcd2b5
desonglll <lindesong666@163.com>
2025-02-16 17:55:49 +0800

    Update

--- frontend/src/components/skeleton/Footer.tsx
+++ frontend/src/components/skeleton/Footer.tsx
@@ -1,56 +1,56 @@
 import type React from "react";
-import { Button, Space } from "antd";
+import {Button, Space} from "antd";
 import "./Footer.scss";
 
 const Footer: React.FC = () => {
-  const handleContact = () => {
-    const email = "info@sd-get.com"; // Target email address
-    const subject = encodeURIComponent("Inquiry About Your Product"); // Email subject (URL encoded)
-    const body = encodeURIComponent(
-      "Hello, I am interested in your product. Please provide me with more information."
-    ); // Email body
+    const handleContact = () => {
+        const email = "info@sd-get.com"; // Target email address
+        const subject = encodeURIComponent("Inquiry About Your Product"); // Email subject (URL encoded)
+        const body = encodeURIComponent(
+            "Hello, I am interested in your product. Please provide me with more information."
+        ); // Email body
 
-    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
-  };
+        window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+    };
 
-  // const handleOrderNow = () => {
-  //   // 在此处理“现在订购”的逻辑，比如跳转到订购页面
-  //   console.log("现在订购");
-  //   navigate(RouterEndpoint.order);
-  // };
+    // const handleOrderNow = () => {
+    //   // 在此处理“现在订购”的逻辑，比如跳转到订购页面
+    //   console.log("现在订购");
+    //   navigate(RouterEndpoint.order);
+    // };
 
-  return (
-    <div className="footer">
-      <div className="footer-content">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-          }}
-        >
-          <div>Tel: 0633-6810000, Mobile: 13606332126</div>
-          <div>General Manager: Manager Li</div>
-          <div>Email: info@sd-get.com</div>
-          <div>
-            Address: Juxian Economic Development Zone, Rizhao City, Shandong
-            Province
-          </div>
-          <div className="footer-info">
-            Shunde China, 2025 All Right Reserved
-          </div>
-        </div>
-      </div>
-      <Space size="middle" className="footer-actions">
-        <Button type="link" onClick={handleContact}>
-          Contact Us
-        </Button>
-        {/* <Button type="primary" onClick={handleOrderNow}>
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        fontSize: 13
+                    }}
+                >
+                    <div>Tel: +61 493307701, +86 15006905606</div>
+                    <div>General Manager: Manager Li</div>
+                    <div>Email: shunde02@sd-get.com</div>
+                    <div>
+                        Address: 3 George Julius Ave, Zetland, Sydney, NSW
+                    </div>
+                    <div className="footer-info">
+                        Shunde China, 2025 All Right Reserved
+                    </div>
+                </div>
+            </div>
+            <Space size="middle" className="footer-actions">
+                <Button type="link" onClick={handleContact}>
+                    Contact Us
+                </Button>
+                {/* <Button type="primary" onClick={handleOrderNow}>
           现在订购
         </Button> */}
-      </Space>
-    </div>
-  );
+            </Space>
+        </div>
+    );
 };
 
 export default Footer;
--- frontend/src/components/top_menu/TopMenu.tsx
+++ frontend/src/components/top_menu/TopMenu.tsx
@@ -1,101 +1,101 @@
-import React, { useEffect, useState } from "react";
-import { Layout, Menu, Image, MenuProps } from "antd";
-import { useNavigate } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Layout, Menu, Image, MenuProps} from "antd";
+import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import "./TopMenu.scss";
 import logo from "../../assets/logo.png";
 import logo1 from "../../assets/logo1.jpg";
-import { RouterEndpoint } from "../../api/router-endpoint";
-import { Information } from "../../props";
+import {RouterEndpoint} from "../../api/router-endpoint";
+import {Information} from "../../props";
 
-const { Header } = Layout;
+const {Header} = Layout;
 
 const TopMenu: React.FC = () => {
-  const [info, setInfo] = useState<Information>({
-    address: "",
-    available: false,
-    company_name: "Shun De",
-    email: "info@sd-get.com",
-    id: 0,
-    manager: undefined,
-    member: [],
-    phone: "15006905606",
-  });
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
+    const [info, setInfo] = useState<Information>({
+        address: "",
+        available: false,
+        company_name: "Shun De",
+        email: "shunde02@sd-get.com",
+        id: 0,
+        manager: undefined,
+        member: [],
+        phone: "+86 15006905606",
+    });
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    axios
-      .get("info/")
-      .then((response) => {
-        if (response.status === 200) {
-          const information = response.data as Information[];
-          information.forEach((item) => {
-            if (item.available) {
-              setInfo(item);
-            }
-          });
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get("info/")
+            .then((response) => {
+                if (response.status === 200) {
+                    const information = response.data as Information[];
+                    information.forEach((item) => {
+                        if (item.available) {
+                            setInfo(item);
+                        }
+                    });
+                }
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
 
-  const menuItems: MenuProps["items"] = [
-    {
-      key: RouterEndpoint.category,
-      label: <a>Category</a>,
-    },
-    {
-      key: "email",
-      label: (
-        <a href={`mailto:${info.email}`} type="link">
-          Mail
-        </a>
-      ),
-    },
-    {
-      key: "phone",
-      label: (
-        <a href={`tel:${info.phone}`} type="link">
-          Phone
-        </a>
-      ),
-    },
-  ];
+    const menuItems: MenuProps["items"] = [
+        {
+            key: RouterEndpoint.category,
+            label: <a>Category</a>,
+        },
+        {
+            key: "email",
+            label: (
+                <a href={`mailto:${info.email}`} type="link">
+                    Mail
+                </a>
+            ),
+        },
+        {
+            key: "phone",
+            label: (
+                <a href={`tel:${info.phone}`} type="link">
+                    Phone
+                </a>
+            ),
+        },
+    ];
 
-  const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
-    navigate(e.key);
-  };
+    const onClick: MenuProps["onClick"] = (e) => {
+        console.log("click ", e);
+        navigate(e.key);
+    };
 
-  if (loading) return null;
+    if (loading) return null;
 
-  return (
-    <>
-      <Header className="top-menu-header">
-        <div className="logo-container" onClick={() => navigate("/")}>
-          <Image src={logo1} alt="Logo" className="logo" preview={false} />
-          <Image
-            src={logo}
-            alt="Logo"
-            className="logo"
-            style={{ width: 108 }}
-            preview={false}
-          />
-        </div>
-        <Menu
-          mode="horizontal"
-          className="top-menu"
-          theme="light"
-          items={menuItems}
-          onClick={onClick}
-        />
-      </Header>
-      {/*<div className="header-placeholder"/>*/}
-    </>
-  );
+    return (
+        <>
+            <Header className="top-menu-header">
+                <div className="logo-container" onClick={() => navigate("/")}>
+                    <Image src={logo1} alt="Logo" className="logo" preview={false} style={{paddingRight: "35px"}}/>
+                    <Image
+                        src={logo}
+                        alt="Logo"
+                        className="logo"
+                        style={{width: 108}}
+                        preview={false}
+                    />
+                </div>
+                <Menu
+                    mode="horizontal"
+                    className="top-menu"
+                    theme="light"
+                    items={menuItems}
+                    onClick={onClick}
+                />
+            </Header>
+            {/*<div className="header-placeholder"/>*/}
+        </>
+    );
 };
 
 export default TopMenu;
